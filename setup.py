c8a5c211f492c029249ee5ae59785da3e9d4104e
AbeMic <122504959+AbeMic@users.noreply.github.com>
2024-04-17 22:38:00 -0500

    Added a .gitignore, requirements.txt, as well as some useful bat files

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='Main',
+      version='0.1',
+      description='main package',
+      license='MIT',
+      packages=['Scripts', 'Scripts.CustomPyQT6UI'],
+      zip_safe=False)
